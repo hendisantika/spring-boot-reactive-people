7dddca93e82dedeebda9d02cdfee72908a0ac1bd
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 09:49:48 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/reactivepeople/SpringBootReactivePeopleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.reactivepeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactivePeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactivePeopleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/reactivepeople/SpringBootReactivePeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.reactivepeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactivePeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
