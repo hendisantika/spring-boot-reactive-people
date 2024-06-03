a0d9600c5c7a38fca74d246d53b6116a4e4601e3
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 09:56:38 +0700

    Add Constants🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/reactivepeople/Constants.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.reactivepeople;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-people
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/3/24
+ * Time: 09:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * This class contains constants that are used throughout the application.
+ */
+public class Constants {
+    /**
+     * The base API endpoint for all people-related operations.
+     */
+    public static final String API = "/api/people";
+}
