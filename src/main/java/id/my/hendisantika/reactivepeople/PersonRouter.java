59a41860208458fd9e5ec63f3de24d2856383422
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 10:09:11 +0700

    Add Swagger Description🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/reactivepeople/PersonRouter.java
+++ src/main/java/id/my/hendisantika/reactivepeople/PersonRouter.java
@@ -11,9 +11,20 @@ package id.my.hendisantika.reactivepeople;
  * To change this template use File | Settings | File Templates.
  */
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -34,7 +45,164 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class PersonRouter {
     @Bean
     @RouterOperations(
-
+            {
+                    @RouterOperation(
+                            path = API,
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.GET,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleFindAll",
+                            operation = @Operation(
+                                    operationId = "handleFindAll",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(
+                                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                                            array = @ArraySchema(
+                                                                    schema = @Schema(implementation = Person.class))
+                                                    )
+                                            )
+                                    }
+                            )
+                    ),
+                    @RouterOperation(
+                            path = API + "/{id}",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.GET,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleFindById",
+                            operation = @Operation(
+                                    operationId = "handleFindById",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Person.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "404", description = "person not found with given id")
+                                    },
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.PATH, name = "id")
+                                    }
+                            )
+                    ),
+                    @RouterOperation(
+                            path = API + "/firstByName/{name}",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.GET,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleFindFirstByName",
+                            operation = @Operation(
+                                    operationId = "handleFindFirstByName",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Person.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "404", description = "no person found with given name")
+                                    },
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.PATH, name = "name")
+                                    }
+                            )
+                    ),
+                    @RouterOperation(
+                            path = API + "/{id}",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.DELETE,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleDeleteById",
+                            operation = @Operation(
+                                    operationId = "handleDeleteById",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = String.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "404", description = "person not found with given id")
+                                    },
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.PATH, name = "id")
+                                    }
+                            )
+                    ),
+                    @RouterOperation(
+                            path = API + "/{id}",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.PUT,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleUpdate",
+                            operation = @Operation(
+                                    operationId = "handleUpdate",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Person.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "404", description = "person not found with given id"),
+                                            @ApiResponse(responseCode = "400", description = "person not not valid")
+                                    },
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.PATH, name = "id")
+                                    },
+                                    requestBody = @RequestBody(
+                                            content = @Content(
+                                                    schema = @Schema(implementation = Person.class)
+                                            )
+                                    )
+                            )
+                    ),
+                    @RouterOperation(
+                            path = API,
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.POST,
+                            beanClass = PersonHandler.class,
+                            beanMethod = "handleCreate",
+                            operation = @Operation(
+                                    operationId = "handleCreate",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "201",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Person.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "400", description = "person not not valid")
+                                    },
+                                    requestBody = @RequestBody(
+                                            content = @Content(
+                                                    schema = @Schema(implementation = Person.class)
+                                            )
+                                    )
+                            )
+                    )
+            }
     )
     RouterFunction<ServerResponse> http(PersonHandler personHandler) {
         return nest(path(API),
