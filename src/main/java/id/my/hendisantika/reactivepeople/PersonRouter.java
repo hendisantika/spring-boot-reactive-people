f1c3bef622298aaccb8bf6142cb90ce86a03715f
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 10:06:36 +0700

    Add http in PersonRouter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/reactivepeople/PersonRouter.java
+++ src/main/java/id/my/hendisantika/reactivepeople/PersonRouter.java
@@ -12,6 +12,17 @@ package id.my.hendisantika.reactivepeople;
  */
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static id.my.hendisantika.reactivepeople.Constants.API;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
  * This class is responsible for routing HTTP requests to the appropriate handler methods.
@@ -19,4 +30,14 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class PersonRouter {
+    RouterFunction<ServerResponse> http(PersonHandler personHandler) {
+        return nest(path(API),
+                route(GET(""), personHandler::handleFindAll)
+                        .andRoute(GET("/{id}"), personHandler::handleFindById)
+                        .andRoute(GET("/firstByName/{name}"), personHandler::handleFindFirstByName)
+                        .andRoute(DELETE("/{id}"), personHandler::handleDeleteById)
+                        .andRoute(POST(""), personHandler::handleCreate)
+                        .andRoute(PUT("/{id}"), personHandler::handleUpdate)
+        );
+    }
 }
