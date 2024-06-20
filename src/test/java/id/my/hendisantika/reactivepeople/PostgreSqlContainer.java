b328e90052a7377a30b8583e9821bb12859c488a
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:41:22 +0700

    Add PostgreSqlContainer🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/reactivepeople/PostgreSqlContainer.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.reactivepeople;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-people
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
+@Testcontainers
+public abstract class PostgreSqlContainer {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17beta1-alpine3.20")
+            .withInitScript("schema.sql")
+            .withReuse(true);
+}
