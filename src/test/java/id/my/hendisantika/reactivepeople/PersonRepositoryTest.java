2e9604c97bca31ea525faf34c9fe613d2a208806
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:40:33 +0700

    Add PersonRepositoryTest🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/reactivepeople/PersonRepositoryTest.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.reactivepeople;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-people
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/20/24
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
+@DataR2dbcTest
+class PersonRepositoryTest extends PostgreSqlContainer {
+
+    @Autowired
+    PersonRepository personRepository;
+
+}
