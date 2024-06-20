483a59e998902f6b7e7fa48c4a655b3a8f858dd0
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:55:00 +0700

    Add setUp in RestDocsTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/reactivepeople/RestDocsTest.java
+++ src/test/java/id/my/hendisantika/reactivepeople/RestDocsTest.java
@@ -1,11 +1,21 @@
 package id.my.hendisantika.reactivepeople;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,5 +44,24 @@ public class RestDocsTest extends PostgreSqlContainer {
                 .findTopByOrderByIdAsc()
                 .block();
         return person;
+    }
+
+    @BeforeEach
+    public void setUp(ApplicationContext applicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext).configureClient()
+                .filter(documentationConfiguration(restDocumentation))
+                .build();
+
+        List<Person> people = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            people.add(new Person("Person@" + i));
+        }
+        Flux<Person> personFlux = this.personRepository
+                .deleteAll()
+                .thenMany(this.personRepository.saveAll(people));
+        StepVerifier
+                .create(personFlux)
+                .expectNextCount(100L)
+                .verifyComplete();
     }
 }
