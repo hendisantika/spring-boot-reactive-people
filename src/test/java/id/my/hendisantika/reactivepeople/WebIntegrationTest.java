e85fa81826753659e0cbf4d6dcbbe2c092a63cb6
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:59:08 +0700

    Add setUp in WebIntegrationTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/reactivepeople/WebIntegrationTest.java
+++ src/test/java/id/my/hendisantika/reactivepeople/WebIntegrationTest.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.reactivepeople;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,20 @@ public class WebIntegrationTest extends PostgreSqlContainer {
 
     @Autowired
     PersonRepository personRepository;
+
+    @BeforeEach
+    public void setUp() {
+        log.info("Running setUp -> creatíng 100 people");
+        ArrayList<Person> people = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            people.add(new Person("Person@" + i));
+        }
+        Flux<Person> personFlux = personRepository
+                .deleteAll()
+                .thenMany(personRepository.saveAll(people));
+        StepVerifier
+                .create(personFlux)
+                .expectNextCount(100L)
+                .verifyComplete();
+    }
 }
