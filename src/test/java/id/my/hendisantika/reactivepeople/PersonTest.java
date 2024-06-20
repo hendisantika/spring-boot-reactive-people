b70fcae3202e682c72f595e5b290b66a65836fa3
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:52:17 +0700

    Add should_create_person in PersonTest🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/reactivepeople/PersonTest.java
+++ src/test/java/id/my/hendisantika/reactivepeople/PersonTest.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.reactivepeople;
 
 import jakarta.validation.Validator;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +21,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PersonTest {
     @Autowired
     private Validator validator;
+
+    @Test
+    void should_create_person() {
+        Person person = new Person(1L, "Gojo");
+        assertThat(person.getId()).isEqualTo(1L);
+        assertThat(person.getName()).isEqualTo("Gojo");
+    }
 
 }
