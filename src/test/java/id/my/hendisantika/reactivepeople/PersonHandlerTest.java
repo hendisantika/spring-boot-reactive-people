eccffac525ad885e1d10a25682011d4c75a064c0
hendisantika <hendisantika@yahoo.co.id>
2024-06-20 13:35:44 +0700

    Add should_handle_find_all🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/reactivepeople/PersonHandlerTest.java
+++ src/test/java/id/my/hendisantika/reactivepeople/PersonHandlerTest.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.reactivepeople;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import static id.my.hendisantika.reactivepeople.Constants.API;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,28 @@ class PersonHandlerTest {
 
     @MockBean
     private PersonRepository personRepository;
+
+    @Test
+    @DisplayName("should handle request find all")
+    void should_handle_find_all() {
+        when(this.personRepository.findAll())
+                .thenReturn(Flux.just(
+                        new Person(1L, "Yuji"),
+                        new Person(2L, "Megumi"),
+                        new Person(3L, "Gojo")
+                ));
+        this.webTestClient
+                .get()
+                .uri(API)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Person.class)
+                .hasSize(3)
+                .contains(
+                        new Person(1L, "Yuji"),
+                        new Person(2L, "Megumi"),
+                        new Person(3L, "Gojo")
+                );
+    }
 }
